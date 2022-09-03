bf3982c1babed0cd4ac00b04307feab6bec9fb92
huttongrabiel <huttonthomas@icloud.com>
2022-09-02 17:02:24 -0700

    Main: Set up basic error propagation
    All errors should propagate back to main with a meaningful error message
    and data that is to be printed. Right now, errors do not accumulate, the
    whole thing dies on each error. Ideally, the errors would accumulate and
    then all be printed at once.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+use ansi_term::Color::Red;
 use jack_compiler::syntax_analyzer;
 
 fn main() {
-
+    match syntax_analyzer::analyzer_main() {
+        Ok(_) => (),
+        Err(e) => println!("{} {}", Red.paint("Error:"), e),
+    };
 }
