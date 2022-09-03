7268bd5838635377a4fb9d901e2f5c4b9168beff
huttongrabiel <huttonthomas@icloud.com>
2022-09-02 17:04:18 -0700

    Lexer: Create TokenType enum and Token struct
    TokenType indicates the basic type of the token according to the Jack
    grammar. Token is the struct that represents all the information a token
    should carry along to the parser.

---
+++ src/lexer.rs
@@ -0,0 +1,33 @@
+use std::fmt::Display;
+
+#[derive(Debug)]
+pub enum TokenType {
+    KeyWord,
+    Symbol,
+    Identifier,
+    IntVal,
+    StringVal,
+}
+
+impl Display for TokenType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let token_name: &str;
+        match self {
+            TokenType::KeyWord => token_name = "KeyWord",
+            TokenType::Symbol => token_name = "Symbol",
+            TokenType::Identifier => token_name = "Identifier",
+            TokenType::IntVal => token_name = "IntVal",
+            TokenType::StringVal => token_name = "StringVal",
+        }
+        write!(f, "{}", token_name.to_string())
+    }
+}
+
+#[derive(Debug)]
+pub struct Token {
+    pub token_type: TokenType,
+    pub value: String,
+    pub path: String,
+    pub line: u64,
+    pub column: u16,
+}
