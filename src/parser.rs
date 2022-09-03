f2df8abb30336c365db69497aa694be7d100b3d5
huttongrabiel <huttonthomas@icloud.com>
2022-09-03 15:27:17 -0700

    Parser: Flush output stream after print!()

--- src/parser.rs
+++ src/parser.rs
@@ -1,11 +1,15 @@
 use crate::lexer::TokenData;
+use std::io;
+use std::io::Write;
 
 pub fn parse(tokens: Vec<TokenData>) -> Result<String, &'static str> {
     let parse_tree = String::new();
 
     for token in tokens {
         print!("<{}>", token.token_type);
-        print!("{}", token.value);
+        io::stdout().flush().unwrap();
+        print!("{:?}", token.value);
+        io::stdout().flush().unwrap();
         println!("</{}>", token.token_type);
     }
 
