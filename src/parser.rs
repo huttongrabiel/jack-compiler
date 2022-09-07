f42ad1fb762532004b10941b1100662ceee694fd
huttongrabiel <huttonthomas@icloud.com>
2022-09-07 09:58:59 -0700

    Parser: Create base parser functions
    These functions should cover all the parsing. Helper functions will of
    course be added as they become necessary. But the main hunk of parsing
    should be covered.

--- src/parser.rs
+++ src/parser.rs
@@ -32,4 +32,37 @@ impl Parser {
 
         Ok(parse_tree)
     }
+
+    fn parse_class(&mut self) {}
+
+    fn parse_class_var_dev(&mut self) {}
+
+    // Subroutine can be a method, function, or constructor
+    fn parse_subroutine(&mut self) {}
+
+    fn parse_parameter_list(&mut self) {}
+
+    fn parse_var_dec(&mut self) {}
+
+    fn parse_statements(&mut self) {}
+
+    fn parse_do(&mut self) {}
+
+    fn parse_let(&mut self) {}
+
+    fn parse_while(&mut self) {}
+
+    fn parse_return(&mut self) {}
+
+    fn parse_if(&mut self) {}
+
+    fn parse_expression(&mut self) {}
+
+    // Will require a peek() function to see type of next token. This is to
+    // distinguish between foo, foo[i], foo.print(), etc.
+    fn parse_term(&mut self) {}
+
+    // Expression lists are lists of expressions separated by commas. They CAN
+    // be empty.
+    fn parse_expression_list(&mut self) {}
 }
