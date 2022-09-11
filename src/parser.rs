8e9a92f6c735a901982f764462f8a3d4461547c2
huttongrabiel <huttonthomas@icloud.com>
2022-09-10 20:28:12 -0700

    Parser: Implement parsing for let statements
    Let statements are statements that look as such:
        'let' VarName('[' expression ']')? '=' expression ';'

--- src/parser.rs
+++ src/parser.rs
@@ -748,7 +748,79 @@ impl Parser {
     }
 
     fn parse_let(&mut self) -> Result<String, JackError> {
-        Ok(String::from(""))
+        let mut let_parse_tree = self.generate_indent();
+
+        writeln!(let_parse_tree, "<{:?}>", ParseTag::LetStatement)
+            .expect("Failed to write <LetStatement.");
+
+        let_parse_tree.push_str(&self.generate_xml_tag());
+        self.index += 1;
+
+        if self.tokens[self.index].token != Token::Identifier {
+            return Err(JackError::new(
+                ErrorType::MissingIdentifier,
+                "Expected identifier in let statement. 'let _identifer_ = ...'",
+                Some(self.tokens[self.index].path.clone()),
+                Some(self.tokens[self.index].line),
+                Some(self.tokens[self.index].column),
+            ));
+        }
+
+        let_parse_tree.push_str(&self.generate_xml_tag());
+        self.index += 1;
+
+        if self.tokens[self.index].token == Token::OpenBracket {
+            let_parse_tree.push_str(&self.generate_xml_tag());
+            self.index += 1;
+
+            let_parse_tree.push_str(&self.parse_do()?);
+
+            if self.tokens[self.index].token != Token::CloseBracket {
+                return Err(JackError::new(
+                    // FIXME: Add ErrorType for this. (ExpectedCloseBracket)
+                    ErrorType::GeneralError,
+                    "Expected ']'.",
+                    Some(self.tokens[self.index].path.clone()),
+                    Some(self.tokens[self.index].line),
+                    Some(self.tokens[self.index].column),
+                ));
+            }
+
+            let_parse_tree.push_str(&self.generate_xml_tag());
+            self.index += 1;
+        }
+
+        if self.tokens[self.index].token != Token::Equal {
+            return Err(JackError::new(
+                // FIXME: Add ErrorType for this. (UnexpectedToken)
+                ErrorType::GeneralError,
+                "Expected '='.",
+                Some(self.tokens[self.index].path.clone()),
+                Some(self.tokens[self.index].line),
+                Some(self.tokens[self.index].column),
+            ));
+        }
+
+        let_parse_tree.push_str(&self.generate_xml_tag());
+        self.index += 1;
+
+        let_parse_tree.push_str(&self.parse_do()?);
+
+        if self.tokens[self.index].token != Token::Semicolon {
+            return Err(JackError::new(
+                // FIXME: Add ErrorType for this. (UnexpectedToken)
+                ErrorType::GeneralError,
+                "Expected ';'.",
+                Some(self.tokens[self.index].path.clone()),
+                Some(self.tokens[self.index].line),
+                Some(self.tokens[self.index].column),
+            ));
+        }
+
+        let_parse_tree.push_str(&self.generate_xml_tag());
+        self.index += 1;
+
+        Ok(let_parse_tree)
     }
 
     fn parse_while(&mut self) -> Result<String, JackError> {
