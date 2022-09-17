5328cc86f2aa3b6c2bc6c89d92de57836973d007
huttongrabiel <huttonthomas@icloud.com>
2022-09-17 15:18:39 -0700

    Codegen: Get rid of codegen struct
    Codegen does not need to be any sort of object. It generates strings and
    that's it.

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,70 +1,57 @@
 use crate::{lexer::Token, parser::Segment};
 use std::fmt::Write;
 
-#[derive(Debug)]
-pub struct Codegen {
-    vm_code: String,
+pub fn gen_push(segment: Segment, index: u32) -> String {
+    let mut push = String::from("push ");
+    writeln!(push, "{:?} {}", segment, index);
+    push
 }
 
-impl Codegen {
-    pub fn new() -> Self {
-        Self {
-            vm_code: String::new(),
-        }
+pub fn gen_pop(segment: Segment, index: u32) -> String {
+    let mut pop = String::from("pop ");
+    writeln!(pop, "{:?} {}", segment, index);
+    pop
+}
+
+pub fn gen_arithmetic(command: Token) -> String {
+    let mut arithmetic_op = "";
+    match command {
+        Token::Plus => arithmetic_op = "add\n",
+        // FIXME: Figure out how to allow negating a number. Right now the
+        // negation won't work because all '-' are treated as sub.
+        Token::Minus => arithmetic_op = "sub\n",
+        Token::Equal => arithmetic_op = "eq\n",
+        Token::GreaterThan => arithmetic_op = "gt\n",
+        Token::LessThan => arithmetic_op = "lt\n",
+        Token::Ampersand => arithmetic_op = "and\n",
+        Token::Pipe => arithmetic_op = "or\n",
+        Token::Tilde => arithmetic_op = "not\n",
+        _ => panic!("Non-arithmetic token given to gen_arithmetic."),
     }
 
-    fn gen_push(segment: Segment, index: u32) -> String {
-        let mut push = String::from("push ");
-        writeln!(push, "{:?} {}", segment, index);
-        push
-    }
+    arithmetic_op.to_string()
+}
 
-    fn gen_pop(segment: Segment, index: u32) -> String {
-        let mut pop = String::from("pop ");
-        writeln!(pop, "{:?} {}", segment, index);
-        pop
-    }
+pub fn gen_label(label: String) -> String {
+    format!("label {}\n", label)
+}
 
-    fn gen_arithmetic(command: Token) -> String {
-        let mut arithmetic_op = "";
-        match command {
-            Token::Plus => arithmetic_op = "add\n",
-            // FIXME: Figure out how to allow negating a number. Right now the
-            // negation won't work because all '-' are treated as sub.
-            Token::Minus => arithmetic_op = "sub\n",
-            Token::Equal => arithmetic_op = "eq\n",
-            Token::GreaterThan => arithmetic_op = "gt\n",
-            Token::LessThan => arithmetic_op = "lt\n",
-            Token::Ampersand => arithmetic_op = "and\n",
-            Token::Pipe => arithmetic_op = "or\n",
-            Token::Tilde => arithmetic_op = "not\n",
-            _ => panic!("Non-arithmetic token given to gen_arithmetic."),
-        }
+pub fn gen_goto(label: String) -> String {
+    format!("goto {}\n", label)
+}
 
-        arithmetic_op.to_string()
-    }
+pub fn gen_if(label: String) -> String {
+    format!("if-goto {}\n", label)
+}
 
-    fn gen_label(label: String) -> String {
-        format!("label {}\n", label)
-    }
+pub fn gen_call(name: String, n_args: u32) -> String {
+    format!("call {} {}\n", name, n_args)
+}
 
-    fn gen_goto(label: String) -> String {
-        format!("goto {}\n", label)
-    }
+pub fn gen_function(name: String, n_locals: u32) -> String {
+    format!("function {} {}\n", name, n_locals)
+}
 
-    fn gen_if(label: String) -> String {
-        format!("if-goto {}\n", label)
-    }
-
-    fn gen_call(name: String, n_args: u32) -> String {
-        format!("call {} {}\n", name, n_args)
-    }
-
-    fn gen_function(name: String, n_locals: u32) -> String {
-        format!("function {} {}\n", name, n_locals)
-    }
-
-    fn gen_return() -> String {
-        String::from("return\n")
-    }
+pub fn gen_return() -> String {
+    String::from("return\n")
 }
